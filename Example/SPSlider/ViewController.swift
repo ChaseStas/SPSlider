57844494da5a66c5b1c5de175678c12a4b78c28f
Stas Parechyn <stasparechin@icloud.com>
2019-03-23 11:36:56 +0300

    Initial commit

---
+++ Example/SPSlider/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SPSlider
+//
+//  Created by ChaseStas on 03/23/2019.
+//  Copyright (c) 2019 ChaseStas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
